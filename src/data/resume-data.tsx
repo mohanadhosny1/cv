74bd6e3b606442fc51431530c1d91beefc9874da
Mohanad Hosny <183147601+mohanadhosny1@users.noreply.github.com>
2024-09-27 21:35:21 +0000

    Updated Resume Data, Added work, edited education, and more skills.

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,6 +1,4 @@
-import {
-  NileShopper
-} from "@/images/logos";
+import { NileShopper } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
@@ -8,8 +6,7 @@ export const RESUME_DATA = {
   initials: "MH",
   location: "Giza, Egypt",
   locationLink: "",
-  about:
-    "Full Stack Engineer dedicated to building high-quality products.",
+  about: "Full Stack Engineer dedicated to building high-quality products.",
   summary:
     "A Full Stack Engineer. Currently, I work mostly with Go, TypeScript, Next.js, and Node.js. I have over 5 years of experience working remotely.",
   avatarUrl: "https://avatars.githubusercontent.com/u/183147601?v=4",
@@ -34,18 +31,32 @@ export const RESUME_DATA = {
     {
       school: "Al Waha International School",
       degree: "High School Degree",
-      start: "2022",
+      start: "2023",
       end: "2024",
     },
   ],
-  work: [],
+  work: [
+    {
+      company: "Freelancer",
+      badges: ["Remote"],
+      title: "Backend Developer and Reverse Engineer",
+      logo: null,
+      start: "2020",
+      end: null,
+      description:
+        "Worked mostly on telegram bots, discord bots, web scrapers, reverse engineering anti-bots, and a lot of other projects.",
+    },
+  ],
   skills: [
+    "Go",
+    "Python",
+    "Web Scraping",
+    "Anti-bot Bypassing",
+    "Reverse Engineering",
     "JavaScript",
     "TypeScript",
     "React/Next.js",
     "Node.js",
-    "Go",
-    "Python",
     "C#",
   ],
   projects: [
@@ -59,7 +70,7 @@ export const RESUME_DATA = {
         "Go",
         "PostgreSQL",
         "Next.js",
-        "TailwindCSS"
+        "TailwindCSS",
       ],
       description:
         "NileShopper is a website to find the best prices for hardware parts in Egypt.",
